1562b3d594c74b6c951d7c600142dedfcd98cec2
DARYA <89320281+darishenko@users.noreply.github.com>
2024-07-04 18:23:27 +0300

    feat: create directive to display info color message about product availability

--- src/app/shared/components/product-info/product-info.component.ts
+++ src/app/shared/components/product-info/product-info.component.ts
@@ -1,15 +1,10 @@
-import {
-  Component,
-  Input,
-  OnChanges,
-  OnInit,
-  SimpleChanges,
-} from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { AddToCartButtonComponent } from '../add-to-cart-button/add-to-cart-button.component';
 import { Product } from '../../models/product/product.model';
 import { RatingComponent } from '../rating/rating.component';
 import { NgIf, NgStyle } from '@angular/common';
 import { ProductReviewsComponent } from '../product-reviews/product-reviews.component';
+import { ProductAvailabilityDirective } from '../../directives/product-availability/product-availability.directive';
 
 @Component({
   selector: 'app-product-info',
@@ -20,46 +15,11 @@ import { ProductReviewsComponent } from '../product-reviews/product-reviews.comp
     NgIf,
     NgStyle,
     ProductReviewsComponent,
+    ProductAvailabilityDirective,
   ],
   templateUrl: './product-info.component.html',
   styleUrls: ['./product-info.component.css'],
 })
-export class ProductInfoComponent implements OnInit, OnChanges {
-  private readonly AVAILABILITY_IN_STOCK = {
-    value: 'In stock',
-    styleClass: 'availability--in-stock',
-  };
-  private readonly AVAILABILITY_OUT_OF_STOCK = {
-    value: 'Out of stock',
-    styleClass: 'availability--out-of-stock',
-  };
-  private readonly AVAILABILITY_ALMOST_SOLD_OUT = {
-    value: 'Almost sold out',
-    styleClass: 'availability--almost-sold-out',
-  };
-
+export class ProductInfoComponent {
   @Input() product!: Product;
-
-  availability!: { value: string; styleClass: string };
-
-  ngOnInit(): void {
-    this.updateAvailability();
-  }
-
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes['product']?.currentValue) {
-      this.updateAvailability();
-    }
-  }
-
-  private updateAvailability(): void {
-    if (this.product) {
-      this.availability =
-        this.product.stock > 10
-          ? this.AVAILABILITY_IN_STOCK
-          : this.product.stock < 1
-            ? this.AVAILABILITY_OUT_OF_STOCK
-            : this.AVAILABILITY_ALMOST_SOLD_OUT;
-    }
-  }
 }
