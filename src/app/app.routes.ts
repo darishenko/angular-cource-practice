7d2267a7b7a027aceab5d33e69949ee5d599379e
DARYA <89320281+darishenko@users.noreply.github.com>
2024-07-09 14:46:00 +0300

    feat: add product edit page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './shared/pages/home/home.component';
 import { ProductDetailsComponent } from './shared/pages/product-details/product-details.component';
+import { ProductEditComponent } from './shared/pages/product-edit/product-edit.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: 'product/edit/:id', component: ProductEditComponent },
   { path: 'product/:id', component: ProductDetailsComponent },
+  { path: '', component: HomeComponent },
 ];
