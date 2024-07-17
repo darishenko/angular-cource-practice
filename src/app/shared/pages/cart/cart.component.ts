db8be07024c454c5b35cec07e64ee769a87ec10f
DARYA <89320281+darishenko@users.noreply.github.com>
2024-07-17 18:49:10 +0300

    feat: add animation for cart page

--- src/app/shared/pages/cart/cart.component.ts
+++ src/app/shared/pages/cart/cart.component.ts
@@ -4,6 +4,23 @@ import { CartService } from '../../services/cart/cart.service';
 import { CartItem } from '../../models/cart-item/cart-item.model';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { faTimes } from '@fortawesome/free-solid-svg-icons/faTimes';
+import { animate, style, transition, trigger } from '@angular/animations';
+
+const hiddenVisible = trigger('hiddenVisible', [
+  transition('void => *', [
+    style({ transform: 'translateX(-100%)', opacity: 0 }),
+    animate(
+      '1.5s ease-in-out',
+      style({ transform: 'translateX(0)', opacity: 1 }),
+    ),
+  ]),
+  transition('* => void', [
+    animate(
+      '1.5s ease-in',
+      style({ transform: 'translateX(100%)', opacity: 0 }),
+    ),
+  ]),
+]);
 
 @Component({
   selector: 'app-cart-item',
@@ -11,6 +28,7 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons/faTimes';
   imports: [NgForOf, FaIconComponent],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css',
+  animations: [hiddenVisible],
 })
 export class CartComponent implements OnInit {
   cartItems: CartItem[] = [];
